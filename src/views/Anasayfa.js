78142409ed3311697e9d35ebd2b1a694f2244efd
gungorn <gungorn@outlook.com>
2020-07-18 19:40:50 +0300

    navigasyon ve anasayfa

---
+++ src/views/Anasayfa.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+class Anasayfa extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>test</Text>
+            </View>
+        );
+    }
+}
+
+export default Anasayfa;
