b8ef4d01f1c482368ac79812a0faed60399ca374
gungorn <gungorn@outlook.com>
2020-08-03 17:02:49 +0300

    footer bileşeni ui

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+import { footerS as S } from '../views/stil';
+import Ikon from './Ikon';
+import temaH from '../helper/temaH';
+
+const Footer = () => {
+    return (
+        <View style={S.K}>
+            <View style={S.solK}>
+                <TouchableOpacity>
+                    <Ikon is={'MaterialCommunityIcons'} i={'react'} c={temaH.renkler.r2} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Ikon is={'MaterialCommunityIcons'} i={'cards-heart'} c={temaH.renkler.r2} />
+                </TouchableOpacity>
+            </View>
+
+            <View style={[S.solK, S.sagK]}>
+                <TouchableOpacity>
+                    <Ikon is={'MaterialCommunityIcons'} i={'account'} c={temaH.renkler.r2} s={36} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Ikon is={'MaterialCommunityIcons'} i={'archive'} c={temaH.renkler.r2} s={30} />
+                </TouchableOpacity>
+            </View>
+
+            <View style={S.anabutonK}>
+                <View style={S.anabutonAK}>
+                    <Ikon is={'MaterialCommunityIcons'} i={'lead-pencil'} s={40} c={temaH.renkler.r2} />
+                </View>
+            </View>
+        </View>
+    );
+}
+
+
+export default Footer;
