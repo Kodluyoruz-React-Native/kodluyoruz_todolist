a648d086d18edb961428fca4e95e1a8441536424
gungorn <gungorn@outlook.com>
2020-08-03 17:04:47 +0300

    not eklemek için modal box ekledim

--- src/views/anasayfa/Anasayfa.js
+++ src/views/anasayfa/Anasayfa.js
@@ -1,24 +1,69 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 import { observer } from 'mobx-react';
-
-import Arkaplan from '../../components/Arkaplan';
+import Modal from 'react-native-modal';
 
 import C from '../../controllers/anasayfa/anasayfaC';
 import splashC from '../../controllers/splashC';
+import temaH from '../../helper/temaH';
 
 import { anasayfaS as S } from '../stil';
-
 
 import UstBolge from './UstBolge';
 import Notlar from './Notlar';
 import Splash from '../Splash';
+
+import Arkaplan from '../../components/Arkaplan';
+import Ikon from '../../components/Ikon';
 
 
 class Anasayfa extends React.Component {
     componentDidMount = C.cDMount;
     componentDidUpdate = C.cDUpdate;
     componentWillUnmount = C.cWUnmount;
+
+
+    notEkleModal() {
+        return (
+            <Modal
+                animationIn={'zoomIn'}
+                animationOut={'zoomOut'}
+                backdropOpacity={0.5}
+                isVisible={false}
+            >
+                <View style={S.notEkleModalK}>
+                    <Text style={S.modalBaslik}>not Oluştur</Text>
+
+                    <TextInput
+                        style={S.baslikInput}
+                        placeholder={'Başlık'}
+                        maxLength={50}
+                    />
+                    <View style={S.notInputK}>
+                        <TextInput
+                            style={S.notInput}
+                            placeholder={'Notunuz'}
+                            multiline
+                        />
+
+                        <View style={S.butonlarK}>
+                            <TouchableOpacity style={S.buton}>
+                                <Ikon is={'MaterialCommunityIcons'} i={'react'} c={temaH.renkler.r2} s={24} />
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style={S.buton}>
+                                <Ikon is={'MaterialCommunityIcons'} i={'share-variant'} c={temaH.renkler.r2} s={24} />
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style={S.buton}>
+                                <Ikon is={'MaterialCommunityIcons'} i={'camera-plus'} c={temaH.renkler.r2} s={24} />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+        );
+    }
 
 
     render() {
@@ -38,6 +83,8 @@ class Anasayfa extends React.Component {
 
 
                 <Splash />
+
+                {this.notEkleModal()}
             </View>
         );
     }
